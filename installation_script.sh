46aac7c79d5436066a5e5bd5a18edd056230e175
anubhavrouter <117714038+anubhavrouter@users.noreply.github.com>
2023-03-14 10:31:05 +0530

    editing script

--- installation_script.sh
+++ installation_script.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
-
-wget https://github.com/anubhavrouter/routerd-release-binary/raw/main/router_binary_release1.tar
-
-cd ~
-
+#!/bin/bash 
 mkdir router-binary
-
+wget https://github.com/anubhavrouter/routerd-release-binary/raw/main/router_binary_release1.tar
 tar -xvf router_binary_release1.tar -C router-binary
-
-#cd router-binary
-echo $PWD
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/router-binary/libs/' >> ~/.bashrc
-. ~/.bashrc
-echo $LD_LIBRARY_PATH
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/router-binary/libs/" >> ~/.bashrc
+source ~/.bashrc
 sudo cp router-binary/routerd /usr/bin/
+exec bash
+rm router_binary_release1.tar
